65d020fcf45346046a4841371e69fc8eff933791
Patrick Flor <pflor@groupon.com>
2013-10-26 12:41:30 -0500

    Solution to week 7 advanced, in Clojure
    See README.md

---
+++ 2013–2014/Week 07/Advanced/pflor/src/pflor/core.clj
@@ -0,0 +1,33 @@
+(ns pflor.core
+  (:require [pflor.word-count :refer [analyze counts]]))
+
+(defn get-text
+  "Gets the contents of a file (relative to project root) into a string.
+   TODO: make this not hold on to the whole file in memory :)"
+  [filepath]
+  (slurp filepath))
+
+(defn count-text
+  "Returns a sorted frequency ArraySeq of words in the text (a single string)"
+  [text]
+  (-> text
+      analyze
+      counts))
+
+(defn print-counts
+  "Prints an ArraySeq of pairs, with evenly-spaced columns"
+  [aseq]
+  (if (seq aseq)
+    (let [max-word-length (apply max (map (comp count first) aseq))]
+      (doseq [[word occurences] aseq]
+        (let [space-buffer (apply str (repeat (- max-word-length (count word)) \space))]
+          (println word space-buffer occurences)))))
+  (println "No words could be found."))
+
+(defn -main
+  [& args]
+  (-> (or (first args) "resources/ex1.txt")
+      get-text
+      count-text
+      print-counts))
+
---
+++ 2013–2014/Week 07/Advanced/pflor/src/pflor/word_count.clj
@@ -0,0 +1,17 @@
+(ns pflor.word-count
+  (:require [clojure.string :refer [lower-case]]))
+
+(defn analyze
+  "Return a seq of the words in the text. Pretty dumb regex to start."
+  [text]
+  (->> text
+       (re-seq #"\w((\-|')?[\w]+)*")
+       (map first)
+       (map lower-case)))
+
+(defn counts
+  "Return a sorted ArraySeq of word -> count for a given seq of words"
+  [word-list]
+  (-> word-list
+      frequencies
+      sort))
