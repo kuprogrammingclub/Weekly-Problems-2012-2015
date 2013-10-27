004c1b82acae14476a78259b8835ac8fdc32188c
Patrick Flor <pflor@groupon.com>
2013-10-27 00:55:40 -0500

    Add a bunch of annoying comments
    Figured it might help explain some of the design choices to folks
    not so familiar with Clojure. Probably would have been better to do
    this with Github comments, but meh.

--- 2013–2014/Week 07/Advanced/pflor/src/pflor/core.clj
+++ 2013–2014/Week 07/Advanced/pflor/src/pflor/core.clj
@@ -1,25 +1,35 @@
 (ns pflor.core
   (:require [pflor.word-count :refer [count-text]]))
 
+;; I stuck the side-effecty stuff in with main.
+
 (defn get-text
   "Gets the contents of a file (relative to project root) into a string.
    TODO: make this not hold on to the whole file in memory :)"
+  ;; NOTE: the way to do so is with java buffery readery things.
   [filepath]
+  ;; A more concise way of defining this function: (def get-text slurp)
   (slurp filepath))
 
 (defn print-counts
   "Prints an ArraySeq of pairs, with evenly-spaced columns"
   [aseq]
+  ;; This is screaming "insert some carriage returns and refactor me!"
+  ;; Clojure should very rarely look this horizontal.
   (if (seq aseq)
     (let [max-word-length (apply max (map (comp count first) aseq))]
-      (doseq [[word occurences] aseq]
+      (doseq [[word occurences] aseq]  ;; destructuring; not as
+                                       ;; powerful as generalized
+                                       ;; pattern matching.
         (let [space-buffer (apply str (repeat (- max-word-length (count word)) \space))]
           (println word space-buffer occurences))))
     (println "No words could be found.")))
 
 (defn -main
   [& args]
-  (-> (or (first args) "resources/ex1.txt")
+  (-> (or (first args) "resources/ex1.txt") ;; first of an empty list?
+                                            ;; Clojure and honey
+                                            ;; badger don't care.
       get-text
       count-text
       print-counts))
--- 2013–2014/Week 07/Advanced/pflor/src/pflor/word_count.clj
+++ 2013–2014/Week 07/Advanced/pflor/src/pflor/word_count.clj
@@ -1,17 +1,36 @@
 (ns pflor.word-count
   (:require [clojure.string :refer [lower-case]]))
 
+;; Notice these are all pure functions. Clojure has immutable core
+;; data structures, such that we can be assured of no side effects
+;; from this namespace in Clojure-land. Can't prove it as in Haskell,
+;; however.
+
 (defn analyze
   "Return a seq of the words in a text. Pretty dumb regex to start."
   [text]
+  ;; The double stabby is a threading macro; they are used to reduce
+  ;; excessive s-expression nesting/indenting and un-reverse its
+  ;; reversed reading-vs-execution ordering. Without threading, this
+  ;; would be:
+  ;; (map lowercase
+  ;;      (map first
+  ;;           (re-seq #"\w((\-|')?[\w]+)*" text)))
+  ;; this is a marginal improvement; it gets better with more s-exps.
   (->> text
-       (re-seq #"\w((\-|')?[\w]+)*")
-       (map first)
-       (map lower-case)))
+       (re-seq #"\w((\-|')?[\w]+)*")  ;; Anyone come up with a better
+                                      ;;   word regexp?
+       (map first)                    ;; Whole match only, not groups
+       (map lower-case)))             ;; Up to you whether or not to
+                                      ;;   ignore capitaliztion; I did
 
 (defn counts
   "Return a sorted ArraySeq of word -> count for a given seq of words"
   [word-list]
+  ;; single-stabby inserts first exp as first element of following
+  ;; sexp, first making non-list forms into lists. double-stabby
+  ;; inserts as /last/ one. Think of it as stack evaluation with
+  ;; partial functions, if you wish.
   (-> word-list
       frequencies
       sort))
@@ -19,6 +38,7 @@
 (defn count-text
   "Returns a sorted frequency ArraySeq of words in a text (a single string)"
   [text]
+  ;; A pretty simple combinator
   (-> text
       analyze
       counts))
