988714c5a9c1c8dd155a09768cc55c2d65c9683d
RyanGlScott <ryan.gl.scott@ku.edu>
2013-10-28 19:20:48 -0500

    string->regex

--- 2013–2014/Week 07/Advanced/rscott/BookReportBot.scala
+++ 2013–2014/Week 07/Advanced/rscott/BookReportBot.scala
@@ -1,21 +1,27 @@
-package com.xnux.sandbox
-
+import scala.collection.GenTraversableOnce
 import scala.collection.SortedMap
 import scala.io.Source
+import scala.util.matching.Regex.MatchIterator
 
 object BookReportBot {
 
-	val jabberwockyRegex = "(\\s+)|(--)" // Specialized for Lewis Carroll's DASHing prose (har har har)
+	/** 
+	 *  I AM OZYMANDIAS, KING OF REGEXES
+	 *  LOOK UPON MY EXPRESSIONS, YE HACKERS, AND DESPAIR
+	 */
+	val jabberwockyRegex = """(\s+)|(--)""".r // Specialized for Lewis Carroll's DASHing prose (har har har)
+	val leadPuncts = """^[^a-zA-Z]+""".r
+	val trailPuncts = "[^a-zA-Z]+$".r // Dolla dolla bill, y'all
 	
 	/** Splits a string into words, using a regex to indicate separators (e.g., spaces, newlines, dashes, etc.) */
-	private def fromFile(path: String): Array[String] = Source.fromFile(path).mkString.split(jabberwockyRegex)
+	private def fromFile(path: String): Array[String] = jabberwockyRegex split Source.fromFile(path).mkString
 	
 	/** Removes leading and trailing punctuation from a word and casts it to lowercase */
-	private def sanitize(word: String): String = word.replaceFirst("^[^a-zA-Z]+", "").replaceAll("[^a-zA-Z]+$", "").toLowerCase
+	private def sanitize(word: CharSequence): String = trailPuncts.replaceAllIn(leadPuncts.replaceFirstIn(word, ""), "").toLowerCase
 	
-	/** Finds the number of occurrences of each word in the array and sorts the word frequencies alphabetically.
+	/** Finds the number of occurrences of each word in the book and sorts the word frequencies alphabetically.
 	 *  NOTE: This casts all words, even proper nouns, to lowercase for simplicity purposes. */
-	private def mapWordFreqs(book: Array[String]): SortedMap[String, Int] = book.foldRight(SortedMap[String, Int]())((word,map) => {
+	private def mapWordFreqs(book: GenTraversableOnce[String]): SortedMap[String, Int] = book.foldRight(SortedMap[String, Int]())((word,map) => {
 		val sWord = sanitize(word)
 		if (sWord.isEmpty) map else map.updated(sWord, map.getOrElse(sWord, 0) + 1)
 	})
