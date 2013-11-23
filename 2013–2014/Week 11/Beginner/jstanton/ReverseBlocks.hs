df43f6d03f3173fcc5e905a53ec80e1108c766b9
jhstanton <jim.h.stanton@gmail.com>
2013-11-23 01:25:28 -0600

    Added haskell beginner solution

--- 2013–2014/Week 11/Beginner/jstanton/ReverseBlocks.hs
+++ 2013–2014/Week 11/Beginner/jstanton/ReverseBlocks.hs
@@ -1,4 +1,4 @@
-revBlocks :: Int -> [a] -> [[a]]
+revBlocks :: Int -> [a] -> [a]
 revBlocks  _ [] = []
-revBlocks k xs = (reverse first) : (revBlocks k rest) where
+revBlocks k xs = (reverse first) ++ (revBlocks k rest) where
   (first, rest) = splitAt k xs
