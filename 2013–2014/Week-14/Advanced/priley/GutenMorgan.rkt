14eaee2d2e1bbc3a6589aebb3ba9d9c0f8afdb6e
Parker Riley <prkriley@gmail.com>
2014-03-04 19:36:36 -0600

    Minor change - advanced solution
    Reversed bad change

--- 2013–2014/Week-14/Advanced/priley/GutenMorgan.rkt
+++ 2013–2014/Week-14/Advanced/priley/GutenMorgan.rkt
@@ -3,12 +3,12 @@
   (if (equal? t 'and)
       'or
       'and))
-(define (deMorg [l (read)])
+(define (deMorg [l (read)]) ;returns the NOT of a given statment, in list form
   (if (list? l)
       (if (equal? (car l) 'not)
           (cadr l)
           (if (null? (cdr l))
               (list 'not (car l))
-              (cons 'not (list (cons (invert (car l)) (map deMorg (cdr l)))))))
+              (cons (invert (car l)) (map deMorg (cdr l)))))
           (list 'not  l)))
 ;example input: (deMorg '(and a (not (or b c))))
