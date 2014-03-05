a8ce1c0ddabdf59d382890a8d6667ed410881ac6
stefanmendoza <brooklynjayhawk@gmail.com>
2014-03-04 21:23:02 -0600

    Fixed solution for uppercase vowels.

--- 2013–2014/Week-14/Beginner/smendoza/disemvoweling.py
+++ 2013–2014/Week-14/Beginner/smendoza/disemvoweling.py
@@ -2,7 +2,7 @@
 #  Author: Stefan Mendoza
 #  Description: Removes all vowels and spaces from user-inputed text, then prints out
 #				both the disemvoweled text and the vowels that were removed
-#  Date Changed: 28 February 2014
+#  Date Changed: 4 March 2014
 
 # creates a list from the user's input
 # creates lists for the final, disemvoweled text and the vowels that are removed
@@ -13,7 +13,7 @@ vowelList = []
 # checks to see if the item at index i is a vowel or space
 # appends the vowels to vowelList and anything that's not a vowel/space to disemvoweledList
 for i in range(0, len(userText)):
-	if userText[i] in ['a', 'e', 'i', 'o', 'u']:
+	if userText[i] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
 		vowelList += userText[i]
 	elif userText[i] != ' ':
 		disemvoweledList += userText[i]
