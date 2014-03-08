e4e0473b010085897966e7112b4d48ee32192965
Aleksander Eskilson <aleksanderesk@gmail.com>
2014-03-08 14:31:09 -0600

    Added prettier solution

--- 2013–2014/Week-14/Beginner/aeskilson/disemvowel.clj
+++ 2013–2014/Week-14/Beginner/aeskilson/disemvowel.clj
@@ -1,11 +1,11 @@
+;; Accepts a collection of space seperated text
+;; Returns a two-element list of vowels and consonants
+(def vowel? (set "aeiouAEIOU"))
+
 (defn disemvowel [word]
-  (let [word-seq (clojure.string/lower-case (.replaceAll word "\\s+" ""))]
-    ((fn [word-seq]
-      (loop [word-seq word-seq vowels '() consons '()]
-        (if (not (nil? (seq word-seq)))
-          (if (re-find #"[aeiou]" (first word-seq))
-            (conj (first word-seq) vowels)
-            (conj (first word-seq) consons))
-          (recur (rest word-seq) vowels consons))))
-     word-seq)
-    (println vowels consons)))
+  (loop [raw (flatten (map seq (clojure.string/split word #"\s+"))), vowels [], consons []]
+    (if (seq raw)
+      (if (vowel? (first raw))
+        (recur (rest raw) (conj vowels (first raw)) consons)
+        (recur (rest raw) vowels (conj consons (first raw))))
+      [(clojure.string/join vowels) (clojure.string/join consons)])))
