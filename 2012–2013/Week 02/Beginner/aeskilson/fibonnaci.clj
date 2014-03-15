b82a6aa05ed64cb7d3678dc86d95d20fe4655fa8
aleksanderesk <aleksanderesk@gmail.com>
2014-03-14 22:18:01 -0500

    Memoized Clojure solution

--- 2012–2013/Week 02/Beginner/aeskilson/fibonnaci.clj
+++ 2012–2013/Week 02/Beginner/aeskilson/fibonnaci.clj
@@ -1,7 +1,8 @@
+;; A memoized Fibonacci summer
 (defn fib [n]
   (if (< n 2) n
     (+ (fib (dec n))
        (fib (dec (dec n))))))
 
 (defn fibSum []
-  (reduce + (take-while (partial > 10000000) (map (memoize fib) (iterate inc 1)))))
+  (reduce + (filter even? (take-while (partial > 10000000) (map (memoize fib) (iterate inc 1))))))
