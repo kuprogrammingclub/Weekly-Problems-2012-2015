8330acf5837ddb171b8656783eed1028cc47677d
RyanGlScott <ryan.gl.scott@ku.edu>
2014-10-05 17:05:54 -0500

    Beginner/Advanced solutions

---
+++ Week05/Beginner/rscott/RPN.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.Char
+import System.Environment
+
+main :: IO ()
+main = getArgs >>= print . rpn
+
+rpn :: [String] -> Int
+rpn = head . foldl rpnFold []
+
+rpnFold :: [Int] -> String -> [Int]
+rpnFold stack []                                = stack
+rpnFold stack str@(c:cs) | length str > 1       = (read str):stack
+                         | null cs && isDigit c = (digitToInt c):stack
+                         | null cs              = case stack of (n1:n2:ns) -> (op c n1 n2):ns
+                         | otherwise            = undefined
+
+op :: Char -> Int -> Int -> Int
+op '+' n1 n2 = n1 + n2
+op '-' n1 n2 = n1 - n2
+op '*' n1 n2 = n1 * n2
+op '^' n1 n2 = n1 ^ n2
+op '/' n1 n2 = if n2 /= 0 then n1 / n2
+op '%' n1 n2 = if n2 /= 0 then n1 `mod` n2
