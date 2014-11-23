5f4eb0d664126b570eff4c0ebdaf9cf64a0f4fdc
Hung Nguyen <hungtn95@gmail.com>
2014-11-23 04:29:45 -0600

    Fixed solution

--- Week11/Advanced/hnguyen/SantaMap.java
+++ Week11/Advanced/hnguyen/SantaMap.java
@@ -1,15 +1,16 @@
 package SantaMap;
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 public class SantaMap {
 	static char[][] map;
 	static boolean[][] visit;
-	static int rows;
-	static int cols;
-	static int startRow;
-	static int startCol;
-	static Stack<Coord> stack = new Stack<Coord>();
-	static Stack result = new Stack();
+	static Coord[][] prev;
+	static Coord cur, start;
+	static int rows, cols, endRow, endCol;
+	static LinkedList<Coord> list = new LinkedList<Coord>();
+	static LinkedList<String> result = new LinkedList<String>();
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		rows = input.nextInt();
@@ -17,87 +18,108 @@ public class SantaMap {
 		input.nextLine();
 		map = new char[rows][cols];
 		visit = new boolean[rows][cols];
+		prev = new Coord[rows][cols];
 		for (int i = 0; i < rows; i++) {
 			String s = input.nextLine();
 			map[i] = s.toCharArray();
+			Arrays.fill(visit[i], false);
+			Arrays.fill(prev[i], new Coord(0, 0));
 			for (int j = 0; j < cols; j++) {
-				visit[i][j] = false;
 				if (map[i][j] == 'S') {
-					startRow = i;
-					startCol = j;
+					start = new Coord(i, j);
 				}
 			}
 		}
-		visit[startRow][startCol] = true;
-		stack.push(new Coord(startRow, startCol));
-		Try(startRow, startCol);
+		walkMaze();
+		print();
 	}
 	
-	 private static class Coord {
-	    	public int myX, myY;
-	    	
-	    	public Coord(int x, int y) {
-	    		myX = x;
-	    		myY = y;
-	    	}
-	    
-	    	@Override
-	    	public boolean equals(Object o) {
-	    		if (o instanceof Coord) {
-	    			Coord c = (Coord) o;
-	    			return this.myX == c.myX && this.myY == c.myY;
-	    		} else {
-	    			return false;
-	    		}
-	    	}
-	 }
-	 
+	private static class Coord {
+    	public int myX, myY;
+    	
+    	public Coord(int x, int y) {
+    		myX = x;
+    		myY = y;
+    	}
+	}
+ 
 	public static void print() {
-		System.out.println("Found");
-		while (!stack.isEmpty()) {
-			Coord coord1 = stack.pop();
-			if (!stack.isEmpty()) {
-				Coord coord2 = stack.peek();
-				if (coord1.myX > coord2.myX) {
-					result.push("South");
-				} else if (coord1.myX < coord2.myX) {
-					result.push("North");
-				} else if (coord1.myY > coord2.myY) {
-					result.push("East");
-				} else if (coord1.myY < coord2.myY) {
-					result.push("West");
-				}
+		int i = endRow;
+		int j = endCol;
+		int temp;
+		while (i != start.myX || j != start.myY) {
+			if (i > prev[i][j].myX) {
+				result.addFirst("South");
 			}
+			if (i < prev[i][j].myX) {
+				result.addFirst("North");
+			} 
+			if (j > prev[i][j].myY) {
+				result.addFirst("East");
+			} 
+			if (j < prev[i][j].myY) {
+				result.addFirst("West");
+			}
+			temp = i;
+			i = prev[temp][j].myX;
+			j = prev[temp][j].myY;
 		}
-		while (!result.isEmpty()) {
-			System.out.print(result.pop() + " ");
+		
+		while (result.size() != 0) {
+			System.out.print(result.removeFirst() + " ");
 		}
 	}
-	public static void Try(int i, int j) {
-		if (map[i][j] == '1') {
-			visit[i][j] = true;
-			stack.push(new Coord(i, j));
-			print();
-			return;
-		} else { 
-			if (i > 0 && map[i-1][j] != '#' && !visit[i-1][j]) {
-				stack.push(new Coord(i-1, j));
-				visit[i-1][j] = true;
-				Try(i-1, j);
-			} else if (i < rows - 1 && map[i+1][j] != '#' && !visit[i+1][j]) {
-				stack.push(new Coord(i+1, j));
-				visit[i+1][j] = true;
-				Try(i+1, j);
-			} else if (j > 0 && map[i][j-1] != '#' && !visit[i][j-1]) {
-				stack.push(new Coord(i, j-1));
-				visit[i][j-1] = true;
-				Try(i, j-1);
-			} else if (j < cols-1 && map[i][j+1] != '#' && !visit[i][j+1]) {
-				stack.push(new Coord(i, j+1));
-				visit[i][j+1] = true;
-				Try(i, j+1);
-			}
+	
+	public static void walkMaze() {
+		move(start);
+		while (!isGoalReached()) {
+			storeValidMoves();
+			if (list.size() == 0) {
+				break;
+			}	
+			move(list.getFirst());
+			list.removeFirst();
 		}
 	}
-
+	
+	public static void storeValidMoves() {
+		int i = cur.myX;
+		int j = cur.myY;
+		Coord x = new Coord(i, j);
+		if (i < rows - 1 && map[i+1][j] != '#' && !visit[i+1][j]) {
+			list.addLast(new Coord(i+1, j));
+			visit[i+1][j] = true;
+			prev[i+1][j] = x;
+		}  
+		if (i > 0 && map[i-1][j] != '#' && !visit[i-1][j]) {
+			list.addLast(new Coord(i-1, j));
+			visit[i-1][j] = true;
+			prev[i-1][j] = x;
+		} 
+		if (j > 0 && map[i][j-1] != '#' && !visit[i][j-1]) {
+			list.addLast(new Coord(i, j-1));
+			visit[i][j-1] = true;
+			prev[i][j-1] = x;
+		}  
+		if (j < cols-1 && map[i][j+1] != '#' && !visit[i][j+1]) {
+			list.addLast(new Coord(i, j+1));
+			visit[i][j+1] = true;
+			prev[i][j+1] = x;
+		}
+	}
+	
+	public static void move(Coord c) {
+		cur = c;
+		visit[cur.myX][cur.myY] = true;
+	}
+	
+	public static boolean isGoalReached() {
+		if (map[cur.myX][cur.myY] == '1') {
+			endRow = cur.myX;
+			endCol = cur.myY;
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
