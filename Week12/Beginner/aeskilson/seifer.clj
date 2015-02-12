4d130be32ec1424cd602d9f25524ef77700911f5
aeskilso <a456e193@ku.edu>
2015-02-11 18:25:21 -0600

    better

--- Week12/Beginner/aeskilson/seifer.clj
+++ Week12/Beginner/aeskilson/seifer.clj
@@ -1,10 +1,9 @@
 ;;;; The Wikipedia article of the day happens to be
 ;;;; https://en.wikipedia.org/wiki/Final_Fantasy_VIII
-(use 'clojure.string)
-(defn cypher [offset]
-  (let [cypher-char (partial (fn [offset letter]
-                               (char (+ (mod (+ (- (int letter) 97) offset) 26) 97))) offset)]
-    (println "This is like talking to a wall:")
-    (when-let [string (read-line)]
-      (reduce str
-              (map cypher-char (seq (lower-case string)))))))
+(defn cypher
+  ([offset]
+   (cypher (read-line) (partial (fn [offset letter]
+                                  (char (+ (mod (+ (- (int letter) 97) offset) 26) 97))) offset)))
+  ([string cypher-fn]
+   (println (reduce str (map cypher-fn string)))
+   (cypher (read-line) cypher-fn)))
