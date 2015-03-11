eeff65f0ef46d9e9b6a31aae50fc1b5ddc0272ef
NanoExplorer <NanoExplorer@users.noreply.github.com>
2015-03-10 23:29:28 -0500

    Advanced Haskell solution
    Added an advanced solution in Haskell. Yes, I've decided to learn Haskell. It makes me feel like a wizard! It took me 50+ lines and several hours to partially solve this problem two years ago, now I can solve it in an hour in a language I've known for a week!

---
+++ Week15/Advanced/crooney/pools.hs
@@ -0,0 +1,10 @@
+pools :: [Int] -> Int
+pools x = poolsLogic 0 0 x
+
+poolsLogic :: Int -> Int -> [Int] -> Int
+poolsLogic index currentTop xs
+  | index == length xs = 0
+  | currentHeight >= currentTop = poolsLogic (index+1) currentHeight xs
+  | currentHeight <  currentTop = (poolHeight - currentHeight) + (poolsLogic (index+1) currentTop xs)
+  where currentHeight = xs !! index
+        poolHeight = min currentTop (maximum (drop (index) xs))
