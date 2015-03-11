e73522741fcf6e013b7ed227abcb0a95d619e09d
NanoExplorer <NanoExplorer@users.noreply.github.com>
2015-03-10 23:59:01 -0500

    formatting

--- Week15/Advanced/crooney/pools.hs
+++ Week15/Advanced/crooney/pools.hs
@@ -3,8 +3,8 @@ pools x = poolsLogic 0 0 x
 
 poolsLogic :: Int -> Int -> [Int] -> Int
 poolsLogic index currentTop xs
-  | index == length xs = 0
+  | index == length xs          = 0
   | currentHeight >= currentTop = poolsLogic (index+1) currentHeight xs
   | currentHeight <  currentTop = (poolHeight - currentHeight) + (poolsLogic (index+1) currentTop xs)
   where currentHeight = xs !! index
-        poolHeight = min currentTop (maximum (drop (index) xs))
+        poolHeight    = min currentTop (maximum (drop (index) xs))
