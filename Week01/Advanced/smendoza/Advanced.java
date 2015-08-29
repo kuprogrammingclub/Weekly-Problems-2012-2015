7aa5fb915b40c276b1608746c17c8c3c04da481d
stefanmendoza <stefanmendoza.dev@gmail.com>
2015-08-29 02:38:23 -0500

    Added advanced Java solution

---
+++ Week01/Advanced/smendoza/Advanced.java
@@ -0,0 +1,26 @@
+// Author: Stefan Mendoza
+// Date: 29 August 2015
+// KU Competitive Programming, Week 01, Advanced Problem
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Advanced {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        BigInteger base = BigInteger.valueOf(2);
+
+        System.out.print("Enter an exponent (the base will be 2): ");
+        int exponent = input.nextInt();
+        String result = base.pow(exponent).toString();
+
+        int sum = 0;
+
+        for(int i = 0; i < result.length(); i++) {
+            sum += Character.getNumericValue(result.charAt(i));
+        }
+
+        System.out.println("The sum of the digits for 2 ^ " + exponent + " is " + sum);
+    }
+}
