b3dafec8cec87363ec038f77b29e46d5da8a4b0f
Adam Mertz <adammertz@gmail.com>
2015-09-09 14:12:51 -0500

    removed a and b vars

--- Week02/Beginner/amertz/funny.py
+++ Week02/Beginner/amertz/funny.py
@@ -3,9 +3,7 @@ while True:
     r = s[::-1]
     funny = True
     for i in range(1, len(s)):
-        a = abs(ord(s[i]) - ord(s[i - 1]))
-        b = abs(ord(r[i]) - ord(r[i - 1]))
-        if a != b:
+        if abs(ord(s[i]) - ord(s[i - 1])) != abs(ord(r[i]) - ord(r[i - 1])):
             funny = False
             break
     print("Funny" if funny else "Not Funny")
