a3c19b33370cf5f2fa05739dc9d873b32f307c4f
Adam Mertz <adammertz@gmail.com>
2015-09-11 23:47:54 -0500

    function based solution

--- Week02/Beginner/amertz/funny.py
+++ Week02/Beginner/amertz/funny.py
@@ -1,9 +1,9 @@
-while True:
+def is_funny():
     s = input('Enter a string: ')
     r = s[::-1]
-    funny = True
     for i in range(1, len(s)):
         if abs(ord(s[i]) - ord(s[i - 1])) != abs(ord(r[i]) - ord(r[i - 1])):
-            funny = False
-            break
-    print("Funny" if funny else "Not Funny")
+            return False
+    return True
+
+print("Funny" if is_funny() else "Not Funny")
