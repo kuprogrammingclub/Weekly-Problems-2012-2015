e5fd6d43f70eb069f2cc892dbedfb6412ded56b2
aleksanderesk <aleksanderesk@gmail.com>
2015-09-16 17:39:26 -0500

    adding problem 2

--- Week01/Beginner/aeskilso/src/aeskilso/core.clj
+++ Week01/Beginner/aeskilso/src/aeskilso/core.clj
@@ -4,3 +4,8 @@
   (println "What's your name?")
   (when-let [name (read-line)]
     (println (str "Hello, " name "!"))))
+
+(defn palindrome [s]
+  (if (= (clojure.string/reverse s) s)
+    "Palindrome"
+    "Not a palindrome"))
