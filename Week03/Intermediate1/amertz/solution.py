bc64d672f7b1f9749478dda1e3081e94cd43317a
Adam Mertz <adammertz@gmail.com>
2015-09-19 00:00:35 -0500

    ternary implementation

--- Week03/Intermediate1/amertz/solution.py
+++ Week03/Intermediate1/amertz/solution.py
@@ -1,9 +1,6 @@
 def sum_list(list_one, list_two):
-    if len(list_one) == 0:
-        list_one.append(0)
-    if len(list_two) == 0:
-        list_two.append(0)
-
+    list_one.append(0) if len(list_one) == 0 else list_one
+    list_two.append(0) if len(list_two) == 0 else list_two
     string_one = ''.join(str(val) for val in list_one[::-1])
     string_two = ''.join(str(val) for val in list_two[::-1])
     total = str(int(string_one) + int(string_two))
