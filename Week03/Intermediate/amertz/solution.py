c31f6fec304d56df03615156bd57c9377dbd84b8
Adam Mertz <adammertz@gmail.com>
2015-09-19 00:07:57 -0500

    uber pythonic update

--- Week03/Intermediate/amertz/solution.py
+++ Week03/Intermediate/amertz/solution.py
@@ -1,6 +1,6 @@
 def sum_list(list_one, list_two):
-    list_one.append(0) if len(list_one) == 0 else list_one
-    list_two.append(0) if len(list_two) == 0 else list_two
+    list_one.append(0) if not len(list_one) else list_one
+    list_two.append(0) if not len(list_two) else list_two
     string_one = ''.join(str(val) for val in list_one[::-1])
     string_two = ''.join(str(val) for val in list_two[::-1])
     total = str(int(string_one) + int(string_two))
