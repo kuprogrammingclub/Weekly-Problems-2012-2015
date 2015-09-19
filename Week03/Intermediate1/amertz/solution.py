97bcb631b60e1faf7b23cbb27dee9d12f27e82e5
Adam Mertz <adammertz@gmail.com>
2015-09-18 20:59:32 -0500

    removed couple lines

--- Week03/Intermediate1/amertz/solution.py
+++ Week03/Intermediate1/amertz/solution.py
@@ -3,11 +3,9 @@ def sum_list(list_one, list_two):
         list_one.append(0)
     if len(list_two) == 0:
         list_two.append(0)
-        
-    list_one = list_one[::-1]
-    list_two = list_two[::-1]
-    string_one = ''.join(str(val) for val in list_one)
-    string_two = ''.join(str(val) for val in list_two)
+
+    string_one = ''.join(str(val) for val in list_one[::-1])
+    string_two = ''.join(str(val) for val in list_two[::-1])
     total = str(int(string_one) + int(string_two))
     total = [int(c) for c in total]
     return total[::-1]
