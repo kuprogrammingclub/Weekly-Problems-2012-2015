32060d8516f34cdc62e73464bfc80c0d4e99e270
Adam Mertz <adammertz@gmail.com>
2015-09-24 00:12:27 -0500

    calculated min distance using a generator which avoids recusion depth issue

--- Week03/Hard/amertz/solution.py
+++ Week03/Hard/amertz/solution.py
@@ -8,8 +8,8 @@ def generate_moves(moves):
     for n in range(moves):
         yield (random(), random())
 
-def find_distance(start, positions, distance):
-    if len(positions) > 0:
+def generate_min_dist(start, positions):
+    while len(positions) > 0:
         index = 0
         min_dist = magnitude(start, positions[index])
         for i in range(len(positions)):
@@ -17,19 +17,16 @@ def find_distance(start, positions, distance):
             if mag < min_dist:
                 min_dist = mag
                 index = i
-        distance += min_dist
+        yield min_dist
         start = positions[index]
         del positions[index]
-        return find_distance(start, positions, distance)
-    else:
-        return distance
+        generate_min_dist(start, positions)
 
 starting_position = (0.5, 0.5)
 moves = input('Enter the number of moves: ')
 positions = [pos for pos in generate_moves(moves)]
-print find_distance(starting_position, positions, 0)
-
+print sum([val for val in generate_min_dist(starting_position, positions)])
 test_case = [(0.9, 0.7), (0.7, 0.7), (0.1, 0.1),
             (0.4, 0.1), (0.6, 0.6), (0.8, 0.8)]
 
-assert find_distance(starting_position, test_case, 0) == 1.6467103925399036, 'False'
+assert sum([val for val in generate_min_dist(starting_position, test_case)]) == 1.6467103925399036, 'False'
